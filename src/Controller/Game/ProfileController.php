8e18728fcc4ce7984d700413107ec326bdc69858
frank9999 <admin@frankprojects.com>
2018-04-27 13:07:55 +0200

    Add template for ProfileController

---
+++ src/Controller/Game/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FrankProjects\UltimateWarfare\Controller\Game;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ProfileController extends BaseGameController
+{
+    /**
+     * XXX TODO: Fix me
+     *
+     * @param Request $request
+     * @param string $playerName
+     * @return Response
+     * @throws \Exception
+     */
+    public function profile(Request $request, string $playerName): Response
+    {
+    }
+}
