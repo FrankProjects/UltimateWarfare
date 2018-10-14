aff84de677f7ed650055c536e8301e8f31d68133
frank9999 <admin@frankprojects.com>
2018-10-14 16:16:35 +0200

    Remove unused variables and DI for FleetActionService

--- src/Controller/Game/FleetController.php
+++ src/Controller/Game/FleetController.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 namespace FrankProjects\UltimateWarfare\Controller\Game;
 
-use FrankProjects\UltimateWarfare\Service\Fleet as FleetService;
-use Symfony\Component\HttpFoundation\Request;
+use FrankProjects\UltimateWarfare\Service\FleetActionService;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 final class FleetController extends BaseGameController
 {
     /**
-     * @param Request $request
      * @return Response
      */
-    public function fleetList(Request $request): Response
+    public function fleetList(): Response
     {
         return $this->render('game/fleetList.html.twig', [
             'player' => $this->getPlayer()
@@ -23,15 +21,14 @@ final class FleetController extends BaseGameController
     }
 
     /**
-     * @param Request $request
      * @param int $fleetId
+     * @param FleetActionService $fleetActionService
      * @return Response
      */
-    public function recall(Request $request, int $fleetId): Response
+    public function recall(int $fleetId, FleetActionService $fleetActionService): Response
     {
-        $fleetService = $this->container->get(FleetService::class);
         try {
-            $fleetService->recall($fleetId, $this->getPlayer());
+            $fleetActionService->recall($fleetId, $this->getPlayer());
             $this->addFlash('success', 'You successfully recalled your troops!');
         } catch (Throwable $e) {
             $this->addFlash('error', $e->getMessage());
@@ -43,15 +40,14 @@ final class FleetController extends BaseGameController
     }
 
     /**
-     * @param Request $request
      * @param int $fleetId
+     * @param FleetActionService $fleetActionService
      * @return Response
      */
-    public function reinforce(Request $request, int $fleetId): Response
+    public function reinforce(int $fleetId, FleetActionService $fleetActionService): Response
     {
-        $fleetService = $this->container->get(FleetService::class);
         try {
-            $fleetService->reinforce($fleetId, $this->getPlayer());
+            $fleetActionService->reinforce($fleetId, $this->getPlayer());
             $this->addFlash('success', 'You successfully reinforced your region!');
         } catch (Throwable $e) {
             $this->addFlash('error', $e->getMessage());
