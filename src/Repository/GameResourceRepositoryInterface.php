18aef6831d3d6ce964c6dc05fca14f33f7669d95
frank9999 <admin@frankprojects.com>
2018-10-19 23:37:19 +0200

    Add GameResource repository

---
+++ src/Repository/GameResourceRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FrankProjects\UltimateWarfare\Repository;
+
+use FrankProjects\UltimateWarfare\Entity\GameResource;
+
+interface GameResourceRepositoryInterface
+{
+    /**
+     * @param int $id
+     * @return GameResource|null
+     */
+    public function find(int $id): ?GameResource;
+
+    /**
+     * @return GameResource[]
+     */
+    public function findAll(): array;
+}
