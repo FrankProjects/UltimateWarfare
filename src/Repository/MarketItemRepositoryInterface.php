ab26a0ae8432445892d4e82655e7e7e47f18af4f
frank9999 <admin@frankprojects.com>
2018-10-24 23:40:08 +0200

    Move MarketController logic to MarketActionService

---
+++ src/Repository/MarketItemRepositoryInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FrankProjects\UltimateWarfare\Repository;
+
+use FrankProjects\UltimateWarfare\Entity\MarketItem;
+use FrankProjects\UltimateWarfare\Entity\World;
+
+interface MarketItemRepositoryInterface
+{
+    /**
+     * @param int $id
+     * @return MarketItem|null
+     */
+    public function find(int $id): ?MarketItem;
+
+    /**
+     * @return MarketItem[]
+     */
+    public function findAll(): array;
+
+    /**
+     * @param World $world
+     * @param string $type
+     * @return array
+     */
+    public function findByWorldMarketItemType(World $world, string $type): array;
+
+    /**
+     * @param MarketItem $marketItem
+     */
+    public function remove(MarketItem $marketItem): void;
+
+    /**
+     * @param MarketItem $marketItem
+     */
+    public function save(MarketItem $marketItem): void;
+}
