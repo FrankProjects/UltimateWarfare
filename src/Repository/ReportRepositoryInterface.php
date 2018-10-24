bbf6abc67b1c73a79d5828138fb060eeaf568e72
frank9999 <admin@frankprojects.com>
2018-10-24 23:43:16 +0200

    Rewrite ReportRepository

---
+++ src/Repository/ReportRepositoryInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FrankProjects\UltimateWarfare\Repository;
+
+use FrankProjects\UltimateWarfare\Entity\Player;
+use FrankProjects\UltimateWarfare\Entity\Report;
+
+interface ReportRepositoryInterface
+{
+    /**
+     * @param int $id
+     * @return Report|null
+     */
+    public function find(int $id): ?Report;
+
+    /**
+     * @return Report[]
+     */
+    public function findAll(): array;
+
+    /**
+     * @param Player $player
+     * @param $type
+     * @param int $limit
+     * @return Report[]
+     */
+    public function findReportsByType(Player $player, int $type, int $limit = 100): array;
+
+    /**
+     * @param Player $player
+     * @param int $limit
+     * @return Report[]
+     */
+    public function findReports(Player $player, int $limit = 100): array;
+
+    /**
+     * @param Report $report
+     */
+    public function save(Report $report): void;
+}
