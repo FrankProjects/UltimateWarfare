367cdb8943d0b6c56a59d547322fd847d925cebe
frank9999 <admin@frankprojects.com>
2018-10-28 02:51:31 +0200

    Remove EntityManager dependency from CountryController

---
+++ src/Repository/Doctrine/DoctrineWorldCountryRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FrankProjects\UltimateWarfare\Repository\Doctrine;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use FrankProjects\UltimateWarfare\Entity\WorldCountry;
+use FrankProjects\UltimateWarfare\Repository\WorldCountryRepository;
+
+final class DoctrineWorldCountryRepository implements WorldCountryRepository
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @var EntityRepository
+     */
+    private $repository;
+
+    /**
+     * DoctrineWorldCountryRepository constructor.
+     *
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $this->entityManager->getRepository(WorldCountry::class);
+    }
+
+    /**
+     * @param int $id
+     * @return WorldCountry|null
+     */
+    public function find(int $id): ?WorldCountry
+    {
+        return $this->repository->find($id);
+    }
+}
