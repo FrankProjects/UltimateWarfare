ebf951dcb57ca04d524fb1b010af8b8fcf7d3acd
frank9999 <admin@frankprojects.com>
2018-11-13 22:00:19 +0100

    Add FederationApplicationRepository and FederationNewsRepository

--- src/Repository/FederationRepository.php
+++ src/Repository/FederationRepository.php
@@ -5,14 +5,34 @@ declare(strict_types=1);
 namespace FrankProjects\UltimateWarfare\Repository;
 
 use FrankProjects\UltimateWarfare\Entity\Federation;
+use FrankProjects\UltimateWarfare\Entity\World;
 
 interface FederationRepository
 {
     /**
      * @param int $id
+     * @param World $world
      * @return Federation|null
      */
-    public function find(int $id): ?Federation;
+    public function findByIdAndWorld(int $id, World $world): ?Federation;
+
+    /**
+     * @param string $name
+     * @param World $world
+     * @return Federation|null
+     */
+    public function findByNameAndWorld(string $name, World $world): ?Federation;
+
+    /**
+     * @param World $world
+     * @return Federation[]
+     */
+    public function findByWorldSortedByRegion(World $world): array;
+
+    /**
+     * @param Federation $federation
+     */
+    public function remove(Federation $federation): void;
 
     /**
      * @param Federation $federation
