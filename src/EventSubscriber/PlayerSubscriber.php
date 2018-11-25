91738c665d231096b9c761879ffb9eeeb186c032
frank9999 <admin@frankprojects.com>
2018-11-25 22:08:28 +0100

    Fix indenting

--- src/EventSubscriber/PlayerSubscriber.php
+++ src/EventSubscriber/PlayerSubscriber.php
@@ -9,7 +9,7 @@ use FrankProjects\UltimateWarfare\Service\GameEngine;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
- use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
