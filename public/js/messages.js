7430164be475aea07cf67777705b1ddf3da6bd73
frank9999 <admin@frankprojects.com>
2018-12-22 18:12:23 +0100

    Add messages.js with selectAll javascript

---
+++ public/js/messages.js
@@ -0,0 +1,8 @@
+/*!
+ * Ultimate Warfare JavaScript Library v1.0
+ * https://ultimate-warfare.com/
+ *
+ * Copyright 2018, Frank Schreuder
+ */
+
+function selectAll(form,checked){for(var i=0;i<form.length;i++)form.elements[i].checked=checked}
