41d18c46edccef20ac478a3a16802d333e60d763
frank9999 <admin@frankprojects.com>
2019-01-20 23:06:46 +0100

    Add GameNews functionality to admin

---
+++ src/Form/Admin/GameNewsType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FrankProjects\UltimateWarfare\Form\Admin;
+
+use FrankProjects\UltimateWarfare\Entity\GameNews;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class GameNewsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'label.title'
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'label.message',
+                'attr' => array('cols' => 70, 'rows' => 8),
+            ])
+            ->add('enabled', CheckboxType::class, [
+                'label' => 'label.enabled',
+                'required' => false
+            ])
+            ->add('mainpage', CheckboxType::class, [
+                'label' => 'label.mainpage',
+                'required' => false
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'label.save'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => GameNews::class,
+            'translation_domain' => 'gamenews'
+        ]);
+    }
+}
--- src/Repository/GameNewsRepository.php
+++ src/Repository/GameNewsRepository.php
@@ -9,6 +9,17 @@ use FrankProjects\UltimateWarfare\Entity\GameNews;
 interface GameNewsRepository
 {
     /**
+     * @param int $id
+     * @return GameNews|null
+     */
+    public function find(int $id): ?GameNews;
+
+    /**
+     * @return GameNews[]
+     */
+    public function findAll(): array;
+
+    /**
      * @return GameNews[]
      */
     public function findActiveMainPageNews(): array;
