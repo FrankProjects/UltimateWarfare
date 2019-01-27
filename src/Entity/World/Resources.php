e9335503cb7b2c8a7c2941d44366afbc91e2ed44
frank9999 <admin@frankprojects.com>
2019-01-27 13:57:41 +0100

    Update world entity to use new doctrine config

---
+++ src/Entity/World/Resources.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FrankProjects\UltimateWarfare\Entity\World;
+
+use FrankProjects\UltimateWarfare\Entity\AbstractGameResources;
+
+class Resources extends AbstractGameResources
+{
+}
