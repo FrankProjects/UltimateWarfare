83dd00a88cd35be5d4375cc14427181a759ec76e
frank9999 <admin@frankprojects.com>
2021-12-02 00:06:37 +0100

    Fix phpstan CI issues

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
     require dirname(__DIR__) . '/config/bootstrap.php';
+    /** @phpstan-ignore-next-line */
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
