17c1044a80785785c22b608e0a5f5871844be206
frank9999 <admin@frankprojects.com>
2022-12-12 00:23:39 +0100

    Add NuclearMissileAttack operation

---
+++ src/Service/OperationEngine/OperationProcessor/NuclearMissileAttack.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FrankProjects\UltimateWarfare\Service\OperationEngine\OperationProcessor;
+
+use FrankProjects\UltimateWarfare\Service\OperationEngine\OperationProcessor;
+
+final class NuclearMissileAttack extends OperationProcessor
+{
+    public function getFormula(): float
+    {
+        $specialOps = $this->getSpecialOps();
+        $guards = $this->getGuards();
+        $total_units = $specialOps + $guards + 1;
+
+        return (3 * $specialOps / (2 * $total_units)) - (3 * $guards / (2 * $total_units)) - $this->operation->getDifficulty(
+            ) + $this->getRandomChance();
+    }
+
+    public function processPreOperation(): void
+    {
+        foreach($this->playerRegion->getWorldRegionUnits() as $worldRegionUnit) {
+            if ($worldRegionUnit->getGameUnit()->getId() === $this->operation->getGameUnit()->getId()) {
+                $worldRegionUnit->setAmount($worldRegionUnit->getAmount() - $this->amount);
+                $this->worldRegionUnitRepository->save($worldRegionUnit);
+            }
+        }
+    }
+
+    public function processSuccess(): void
+    {
+        foreach ($this->region->getWorldRegionUnits() as $worldRegionUnit) {
+            $this->worldRegionUnitRepository->remove($worldRegionUnit);
+        }
+
+        foreach ($this->region->getConstructions() as $construction) {
+            $this->constructionRepository->remove($construction);
+        }
+
+        $this->region->setState(1);
+        $this->region->setPlayer(null);
+        $this->worldRegionRepository->save($this->region);
+
+        $reportText = "{$this->playerRegion->getPlayer()->getName()} launched a nuclear missile attack against region {$this->region->getX()}, {$this->region->getY()} and destroyed everything.";
+        $this->reportCreator->createReport($this->region->getPlayer(), time(), $reportText);
+
+        $this->addToOperationLog("The region is fully destroyed, a high amount of toxic radiation will make the region unliveable for an unknown amount of time!");
+    }
+
+    public function processFailed(): void
+    {
+        $specialOpsLost = intval($this->getSpecialOps() * 0.05);
+
+        foreach ($this->playerRegion->getWorldRegionUnits() as $worldRegionUnit) {
+            if ($worldRegionUnit->getGameUnit()->getId() === self::GAME_UNIT_SPECIAL_OPS_ID) {
+                $worldRegionUnit->setAmount(intval($worldRegionUnit->getAmount() - $specialOpsLost));
+                $this->worldRegionUnitRepository->save($worldRegionUnit);
+            }
+        }
+
+        $reportText = "{$this->playerRegion->getPlayer()->getName()} tried to launch a nuclear missile attack on region {$this->region->getX()}, {$this->region->getY()} but failed.";
+        $this->reportCreator->createReport($this->region->getPlayer(), time(), $reportText);
+
+        $this->addToOperationLog("We failed to our nuclear missile attack and lost {$specialOpsLost} Special Ops");
+    }
+
+    public function processPostOperation(): void
+    {
+        $player = $this->region->getPlayer();
+        $player->getNotifications()->setAttacked(true);
+        $this->playerRepository->save($player);
+    }
+}
