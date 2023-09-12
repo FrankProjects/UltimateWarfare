d9b8187fc3f8d36d14799a9b845882a631d458f7
Thorsten <thorsten.daners@antwerpes.de>
2023-09-12 20:24:57 +0200

    GitHub Action using panther.

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
---
+++ tests/PantherBlogPostTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FrankProjects\UltimateWarfare\Tests;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class PantherBlogPostTest extends PantherTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+}
