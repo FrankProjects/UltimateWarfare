9afdec2e1e811566027bd2e5d9a84ebf49529df2
Thorsten <thorsten.daners@antwerpes.de>
2023-09-13 08:30:34 +0200

    Revert testing stuff for now.

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
