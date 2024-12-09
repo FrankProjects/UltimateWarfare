6e485ef1c5124a096ea0a7c2ae8ba617992feaec
Frank Schreuder <admin@frankprojects.com>
2024-12-09 15:17:47 +0100

    Fix typehinting tests/object-manager.php

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -7,7 +7,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 (new Dotenv())->bootEnv(__DIR__ . '/../.env');
 
+
+/** @var string $appEnv */
 $appEnv = $_SERVER['APP_ENV'];
-$kernel = new Kernel((string) $appEnv, (bool) $_SERVER['APP_DEBUG']);
+$kernel = new Kernel($appEnv, (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
 return $kernel->getContainer()->get('doctrine')->getManager();
